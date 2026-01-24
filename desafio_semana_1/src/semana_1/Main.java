6e68d244a4e0093667087f4c655e71d995c79547
waldyr06 <waldyrfelype2012@gmail.com>
2026-01-23 23:15:21 -0300

    Correção de bugs e opção de mandar um arquivo 100% completa com métodos funcionais

--- desafio_semana_1/src/semana_1/Main.java
+++ desafio_semana_1/src/semana_1/Main.java
@@ -1,20 +1,21 @@
 package semana_1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
         AnaliseNumerica analise = new AnaliseNumerica();
+        Scanner teclado = new Scanner(System.in);
 
         System.out.println("------------------------------------------------------------------");
-        System.out.println("Olá, você agora tem um programa para analisar uma lista de números");
-
-        Scanner teclado  = new Scanner(System.in);
+        System.out.println("Olá, você agora tem um programa para analisar uma lista de números!");
 
         boolean rodando = true;
 
-        while (rodando == true){
+        while (rodando == true) {
             System.out.println("------------------------------------------------------------------");
             System.out.println("Você tem duas opções para analisar uma lista de números");
             System.out.println("------------------------------------------------------------------");
@@ -25,37 +26,111 @@ public class Main {
             System.out.println("------------------------------------------------------------------");
 
             int escolha = teclado.nextInt();
-            if (escolha == 1){
-                System.out.println("Escolhas: 1 - ADICIONAR NúMERO / 2 - VISUALIZAR LISTA / " +
-                        "3 - CALCULAR SOMAR / 4 - CALCULAR MÉDIA / 5 - MAIOR NUMERO / 6 - MENOR NUMERO");
+            teclado.nextLine();
+
+            if (escolha == 1) {
+                System.out.println("Escolhas: 1 - ADICIONAR NÚMERO / 2 - VISUALIZAR LISTA / " +
+                        "3 - CALCULAR O TOTAL / 4 - CALCULAR A MÉDIA / 5 - ENCONTRE O MAIOR NUMERO / 6 - ENCONTRE O MENOR NUMERO");
                 System.out.println("------------------------------------------------------------------");
+
                 int escolha2 = teclado.nextInt();
-                //Fazer um switch case aqui
-                if (escolha2 == 1){
-                    System.out.print("Adicionando o número:  ");
-                    double numero = teclado.nextDouble();
-                    analise.adicionarNumero(numero);
+                switch (escolha2) {
+                    case 1 -> {
+                        System.out.print("Adicionando o número:  ");
+                        double numero = teclado.nextDouble();
+                        analise.adicionarNumero(numero);
+                    }
+                    case 2 -> {
+                        System.out.println("Lista: ");
+                        analise.visualizarLista();
+                    }
+                    case 3 -> {
+                        System.out.println("A soma de todos os número é: ");
+                        analise.calcularSoma();
+                    }
+                    case 4 -> {
+                        System.out.println("Média dos números: ");
+                        analise.calcularMedia();
+                    }
+                    case 5 -> {
+                        System.out.println("Maior número encontrado: ");
+                        analise.encontrarMaior();
+                    }
+                    case 6 -> {
+                        System.out.println("Menor número encontrado ");
+                        analise.encontrarMenor();
+                    }
                 }
-                if (escolha2 == 2){
-                    System.out.println("Lista: ");
+            }
+
+            if (escolha == 2) {
+
+                System.out.print("Digite o caminho do arquivo .txt: ");
+                String caminho = teclado.nextLine();
+
+                File arquivo = new File(caminho);
+
+                try {
+                    Scanner leitor = new Scanner(arquivo);
+
+                    int contador = 0;
+                    while (leitor.hasNext()) {
+                        try {
+                            String linha = leitor.next();
+                            double valorConvertido = Double.parseDouble(linha);
+                            analise.adicionarNumero(valorConvertido);
+                        } catch (NumberFormatException e) {
+                            contador++;
+                        }
+                    }
+
+                    System.out.println("---------------------------");
+                    System.out.println("Arquivo lido com sucesso!");
+                    System.out.println("Essa foi a quantidade de palavras que detectamos no seu .txt");
+                    System.out.printf(String.valueOf(contador));
+                    System.out.println("");
+                    System.out.println("---------------------------");
+                    System.out.println("Aqui tá os números contidos nesse  .txt: ");
                     analise.visualizarLista();
+
+                    System.out.println("Escolhas: 1 - ADICIONAR NúMERO / 2 - VISUALIZAR LISTA / " +
+                            "3 - CALCULAR SOMAR / 4 - CALCULAR MÉDIA / 5 - MAIOR NUMERO / 6 - MENOR NUMERO");
+                    System.out.println("------------------------------------------------------------------");
+
+                    int escolha3 = teclado.nextInt();
+                    switch (escolha3) {
+                        case 1 -> {
+                            System.out.print("Adicionando o número:  ");
+                            double numero = teclado.nextDouble();
+                            analise.adicionarNumero(numero);
+                        }
+                        case 2 -> {
+                            System.out.println("Lista: ");
+                            analise.visualizarLista();
+                        }
+                        case 3 -> {
+                            System.out.println("A soma de todos os número é: ");
+                            analise.calcularSoma();
+                        }
+                        case 4 -> {
+                            System.out.println("Média dos números: ");
+                            analise.calcularMedia();
+                        }
+                        case 5 -> {
+                            System.out.println("Maior número encontrado: ");
+                            analise.encontrarMaior();
+                        }
+                        case 6 -> {
+                            System.out.println("Menor número encontrado ");
+                            analise.encontrarMenor();
+                        }
+                    }
+                }  catch (FileNotFoundException e){
+                    System.out.println("ERRO: Arquivo não encontrado!!!");
                 }
-                if (escolha2 == 3){
-                    System.out.println("A soma de todos os número é: ");
-                    analise.calcularSoma();
-                }
-                if (escolha2 == 4){
-                    System.out.println("Média dos números: ");
-                    analise.calcularMedia();
-                }
-                if (escolha2 == 5){
-                    System.out.println("Maior número encontrado: ");
-                    analise.encontrarMaior();
-                }
-                if (escolha2 == 6){
-                    System.out.println("Menor número encontrado ");
-                    analise.encontrarMenor();
-                }
+            }
+            if (escolha == 0){
+                rodando = false;
             }
         }
     }
